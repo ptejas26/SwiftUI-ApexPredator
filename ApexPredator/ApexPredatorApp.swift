e7bf550ef97c5d27f21adb3ff1337148459574b4
ptejas26 <ptejas.patelia@gmail.com>
2023-05-27 22:37:52 -0400

    Initial Commit

---
+++ ApexPredator/ApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorApp.swift
+//  ApexPredator
+//
+//  Created by Tejas on 2023-05-27.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
