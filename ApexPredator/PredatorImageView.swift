b9d31c32aae448a208f484aec22cdcedb437bc89
ptejas26 <ptejas.patelia@gmail.com>
2023-06-17 00:27:58 -0400

    Completed the code challenge

---
+++ ApexPredator/PredatorImageView.swift
@@ -0,0 +1,23 @@
+//
+//  PredatorImageView.swift
+//  ApexPredator
+//
+//  Created by Tejas on 2023-06-11.
+//
+
+import SwiftUI
+
+struct PredatorImageView: View {
+//    let predatorImage: String
+    var body: some View {
+//        Image(predatorImage)
+//            .resizable()
+//            .scaledToFit()
+        Text("Lets do it")
+    }
+}
+
+#Preview {
+//    PredatorImageView(predatorImage: "dimorphodon")
+    PredatorImageView()
+}
